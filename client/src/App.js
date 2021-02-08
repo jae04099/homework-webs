c07651e7af2cfcf4f8d743b73b580eaaead982ec
GyuriLee <jae04099@gmail.com>
2021-02-08 23:30:07 +0900

    mysql node 연동

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>CRUD APP</h1>
+      <div className="form">
+        <label>Movie Name:</label>
+        <input type="text" name="movieName"/>
+        <label>Review:</label>
+        <input type="text" name="review"/>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "Lc0711zz",
+    database: "collegewebdb",
+});
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    
 });
 
 app.listen(3001, () => {
