03985e1215d5547d1e2879f6db3e8cbf4fe3eb9b
GyuriLee <jae04099@gmail.com>
2021-02-13 01:04:25 +0900

    url_ogimage 크기 조절

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -14,11 +14,12 @@ const Card = ({cardData}) => {
                     </div>
                 </div>
                 <div className="desc">
+                    <div className="img-wrap">
                     { cardData.web_og == 'null' ? <div className="no_og"><a href={cardData.web_url}>사이트 바로가기</a></div> : <img
                         src={cardData.web_og}
                         alt="web_og_image"
-                        style={{ maxWidth: 260 + "px", maxHeight: 157 + "px" }}
                     />}
+                    </div>
                     <p>
                         {cardData.web_desc}
                     </p>
