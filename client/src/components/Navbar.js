1085758bd062a3b9bf95afefe3b211b0b99b8ab1
gyuri lee <gyurilee@gyuriui-MacBookAir.local>
2021-10-05 17:15:38 +0900

    add slider

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,14 +8,14 @@ const Navbar = () => {
             <div className="logo">
                 <h4>과제좀<span>해</span></h4>
             </div>
-            <ul className="navlinks">
+            {/* <ul className="navlinks">
                 <li>
                     <a href="#">미래의</a>
                 </li>
                 <li>
                     <a href="#">메뉴</a>
                 </li>
-            </ul>
+            </ul> */}
             
         </nav>
         </>
